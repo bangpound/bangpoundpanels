df2cc6ad17b23b12a11026f6572b8b96ee62d125
Benjamin Doherty <bjd@pobox.com>
2012-04-22 16:21:37 -0500

    initial commit.

---
+++ templates/panels-add-content-link--preview.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Template to control the add content individual links in the add content modal.
+ */
+?>
+<div class="content-type-button clearfix">
+  <?php print $image_button; ?> <?php print $text_button; ?>
+</div>
