90c8d085c2f54826daf3381e4813f07012c1e48e
Benjamin Doherty <bjd@pobox.com>
2012-04-24 19:49:02 -0500

    simplify jquery ui tabs plugin.

--- plugins/styles/tabs/tabs.js
+++ plugins/styles/tabs/tabs.js
@@ -1,19 +1,21 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, undef:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50 */
+/*global Drupal:false, jQuery:false */
 (function ($) {
 
-/**
- * JS related to the tabs in the Panels tabs.
- */
-Drupal.behaviors.panelsTabs = {
-  attach: function (context) {
-    var tabsID = Drupal.settings.panelsTabs.tabsID;
+  /**
+   * JS related to the tabs in the Panels tabs.
+   */
+  Drupal.behaviors.panelsTabs = {
+    attach: function (context, settings) {
+      $.each(settings.panelsTabs, function (index) {
+        var $tabs = $(index, context).prepend('<ul></ul>').tabs(this);
 
-    for (var key in Drupal.settings.panelsTabs.tabsID) {
-      $('#' + tabsID[key] +':not(.tabs-processed)', context)
-        .addClass('tabs-processed')
-        .tabs();
+        $('.panel-pane', $tabs).each(function (index) {
+          var href = '#' + $(this).attr('id'),
+            label = $(this).attr('title');
+          $tabs.tabs('add', href, label, index);
+        });
+      });
     }
-  }
-};
-
+  };
 })(jQuery);
-
