efa8bcec602e17272b18acf99141f2d47072f820
Benjamin Doherty <bjd@pobox.com>
2012-04-24 19:51:36 -0500

    improve accordion plugin.

--- plugins/styles/accordion/accordion.js
+++ plugins/styles/accordion/accordion.js
@@ -1,11 +1,16 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, undef:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50 */
+/*global Drupal:false, jQuery:false */
 (function ($) {
 
-Drupal.behaviors.PanelsAccordionStyle = {
-  attach: function (context, settings) {
-    $.each(Drupal.settings.accordion, function (index) {
-      $(index).accordion(this);
-    });
-  }
-}
+  /**
+   * JS related to the tabs in the Panels tabs.
+   */
+  Drupal.behaviors.PanelsAccordionStyle = {
+    attach: function (context, settings) {
+      $.each(settings.accordion, function (index) {
+        $(index).accordion(this);
+      });
+    }
+  };
 
 })(jQuery);
