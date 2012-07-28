014a2906420ee766f78197f37433ca48a85910bc
Benjamin Doherty <bjd@pobox.com>
2012-07-27 22:53:50 -0500

    remove unreliable preview code.

--- templates/panels-add-content-link--preview.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Template to control the add content individual links in the add content modal.
- */
-?>
-<div class="content-type-button clearfix">
-  <?php print $image_button; ?> <?php print $text_button; ?>
-</div>
